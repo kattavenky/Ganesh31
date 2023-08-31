e3b1df5881be6418a731dfa620b4c566a4d1b58d
Devi K V <59989671+kattavenky@users.noreply.github.com>
2023-08-31 11:42:15 +0530

    Add files via upload

---
+++ Aug31Console/Aug31Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aug31Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("today is Aug 31 Thrusday");
+        }
+    }
+}
